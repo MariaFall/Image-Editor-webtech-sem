9a2132c902cd4fea0b2314519cb9c7d70da499ba
Maria <maria.fall225@proton.me>
2026-05-20 23:26:55 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,25 @@
+// sw.js
+const CACHE_NAME = 'image-editor-v1';
+const ASSETS = [
+    '/',
+    '/index.html',
+    '/app.js',
+    '/worker.js',
+    '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(ASSETS);
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
